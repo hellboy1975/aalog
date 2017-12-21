846afc392631e40dc2387d072379b5a152392ee6
Matt Smith <hellboy1975@gmail.com>
2017-12-21 22:30:15 +1030

    Customise setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='aalog',
+    version='0.1',
+    py_modules=['aalog'],
+    include_package_data=True,
+    description='Adventurous Activities Log',
+    long_description=readme,
+    author='Matt Smith',
+    author_email='hellboy1975@gmail.com',
+    url='https://github.com/hellboy1975/aalog',
+    license=license,
+    entry_points='''
+        [console_scripts]
+        aalog=aalog:cli
+    ''',
+)
