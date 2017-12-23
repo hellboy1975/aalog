208cc64d31384bf5373f6bb5d96243c93cf4fc12
Matt Smith <hellboy1975@gmail.com>
2017-12-23 23:18:25 +1030

    Shift code to aalog package directory

--- aalog/cli.py
+++ aalog/cli.py
@@ -1,10 +1,11 @@
 """
-This is some serious shit
+Contains the entry points for the aalog cli
 """
 import click
+# import aalog
 
 @click.command()
-@click.option('--string', default='world!')
+@click.option('--string', default='cavers!')
 def cli(string):
     """
     This function is a simple test function, that I'm currently mucking
